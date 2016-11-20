bb32a1f0fe8a23e63ab70bbca23c04cfbe8b75f3
Nekomata <irfan.dahir@protonmail.com>
2016-11-20 15:09:46 +0500

    fix of my previous fix
    used `id` instead of `_tbl[2]`

--- yates_sayCommands.lua
+++ yates_sayCommands.lua
@@ -617,7 +617,7 @@ function yates.func.say.kill()
 		return 1
 	end
 
-	if player(id, "health") == 0 then
+	if player(_tbl[2], "health") == 0 then
 		yatesMessage(_id, "You can not kill a dead player!", "warning")
 		return 1
 	end
