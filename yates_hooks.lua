5d0ee1e8e8a509e55aeab2f3af83714b2a7e1b01
Thomas Yates <thomasyates05@gmail.com>
2017-01-24 15:58:48 +0100

    Update yates_hooks.lua

--- yates_hooks.lua
+++ yates_hooks.lua
@@ -151,6 +151,7 @@ addhook("hostagerescue", "yates.hook.hostagerescue")
 
 function yates.hook.join(id)
 	yates.player[id] = {}
+	yates.player[id].say = true
 	yates.player[id].prefix = true
 	yates.player[id].god = false
 	yates.player[id].mute_time = 0
